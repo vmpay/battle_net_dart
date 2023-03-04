673dc45106b9d31401c09347196219109137ea96
Andrew <vmpay.eu@gmail.com>
2023-03-04 20:33:39 +0100

    write_credentials.sh credentials folder update

--- .github/workflows/write_credentials.sh
+++ .github/workflows/write_credentials.sh
@@ -3,4 +3,4 @@
 # Remove old file first
 rm test/integration/credentials.dart
 # Create a replacement
-printf "const String clientId = '%s';\nconst String clientSecret = '%s';" "$CLIENT_ID" "$CLIENT_SECRET" >> test/credentials.dart
+printf "const String clientId = '%s';\nconst String clientSecret = '%s';" "$CLIENT_ID" "$CLIENT_SECRET" >> test/integration/credentials.dart
